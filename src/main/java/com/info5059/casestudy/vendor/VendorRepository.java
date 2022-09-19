ca6cc918f5e264966250b0f452167da9afcf2adc
Mahan <mahanmahdipour1379@gmail.com>
2022-09-19 17:37:53 -0400

    Lab 5 finished

--- src/main/java/com/info5059/casestudy/vendor/VendorRepository.java
+++ src/main/java/com/info5059/casestudy/vendor/VendorRepository.java
@@ -1,5 +1,9 @@
 package com.info5059.casestudy.vendor;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
@@ -9,4 +13,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @Repository
 @RepositoryRestResource(collectionResourceRel = "vendors", path="vendors")
 public interface VendorRepository extends CrudRepository<Vendor, Long>{
+    // extend so we can return the number of rows deleted
+    @Modifying
+    @Transactional
+    @Query("delete from Vendor where id = ?1")
+    int deleteOne(Long vendorid);
 }
