7aa2fb16f3627ba25b39f1133a6710929aa10037
Mahan <mahanmahdipour1379@gmail.com>
2022-10-12 13:40:28 -0400

    Lab 9 finished

--- src/main/java/com/info5059/casestudy/product/ProductRepository.java
+++ src/main/java/com/info5059/casestudy/product/ProductRepository.java
@@ -1,13 +1,13 @@
 package com.info5059.casestudy.product;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, String> {
+public interface ProductRepository extends JpaRepository<Product, String> {
     // will return the number of rows deleted
     @Modifying
     @Transactional
