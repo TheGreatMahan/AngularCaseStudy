94b402ed2e0d4d661d069b5694a4c79e7c4a1f62
Mahan <mahanmahdipour1379@gmail.com>
2022-10-18 22:19:29 -0400

    Lab 11 Finished

---
+++ src/main/java/com/info5059/casestudy/purchaseorder/PurchaseOrderPDFController.java
@@ -0,0 +1,52 @@
+package com.info5059.casestudy.purchaseorder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.info5059.casestudy.product.ProductRepository;
+import com.info5059.casestudy.vendor.VendorRepository;
+import com.itextpdf.io.exceptions.IOException;
+
+import java.io.ByteArrayInputStream;
+
+import javax.servlet.http.HttpServletRequest;
+
+@CrossOrigin
+@RestController
+public class PurchaseOrderPDFController {
+        @Autowired
+        private VendorRepository vendorRepository;
+        @Autowired
+        private ProductRepository productRepository;
+        @Autowired
+        private PurchaseOrderRepository purchaseOrderRepository;
+
+        @RequestMapping(value = "/PurchaseOrderPDF", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
+        public ResponseEntity<InputStreamResource> streamPDF(HttpServletRequest request)
+                        throws IOException, java.io.IOException {
+                // get formatted pdf as a stream
+                {
+                        String repid = request.getParameter("repid");
+                        ByteArrayInputStream bis = PurchaseOrderPDFGenerator.generatePurchaseOrder(repid,
+                                        purchaseOrderRepository,
+                                        vendorRepository,
+                                        productRepository);
+
+                        // ByteArrayInputStream bis = Generator.generateReport();
+                        HttpHeaders headers = new HttpHeaders();
+                        headers.add("Content-Disposition", "inline; filename=poBDF.pdf");
+                        // dump stream to browser
+                        return ResponseEntity
+                                        .ok()
+                                        .headers(headers)
+                                        .contentType(MediaType.APPLICATION_PDF)
+                                        .body(new InputStreamResource(bis));
+                }
+        }
+}
---
+++ src/main/java/com/info5059/casestudy/purchaseorder/PurchaseOrderPDFGenerator.java
@@ -0,0 +1,280 @@
+package com.info5059.casestudy.purchaseorder;
+
+import com.info5059.casestudy.product.Product;
+import com.info5059.casestudy.product.ProductRepository;
+import com.info5059.casestudy.vendor.Vendor;
+import com.info5059.casestudy.vendor.VendorRepository;
+import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.borders.Border;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.colors.ColorConstants;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.properties.TextAlignment;
+import com.itextpdf.layout.properties.UnitValue;
+import org.springframework.web.servlet.view.document.AbstractPdfView;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.net.URL;
+import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public abstract class PurchaseOrderPDFGenerator extends AbstractPdfView {
+        public static ByteArrayInputStream generatePurchaseOrder(String repid,
+                        PurchaseOrderRepository purchaseOrderRepository,
+                        VendorRepository vendorRepository,
+                        ProductRepository productRepository)
+                        throws IOException {
+                URL imageUrl = PurchaseOrderPDFGenerator.class.getResource("/static/images/logo.jpg");
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                PdfWriter writer = new PdfWriter(baos);
+                // Initialize PDF document to be written to a stream not a file
+                PdfDocument pdf = new PdfDocument(writer);
+                // Document is the main object
+                Document document = new Document(pdf);
+                PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA);
+                // PageSize pg = PageSize.A4;
+                Image img = new Image(ImageDataFactory.create(imageUrl)).scaleAbsolute(120, 120)
+                                .setFixedPosition(50, 700);
+                document.add(img);
+                // now let's add a big heading
+                document.add(new Paragraph("\n\n"));
+                Locale locale = new Locale("en", "US");
+                NumberFormat formatter = NumberFormat.getCurrencyInstance(locale);
+                BigDecimal tot = new BigDecimal(0.0);
+                BigDecimal ExtPricetot = new BigDecimal(0.0);
+                BigDecimal tax = new BigDecimal(0.13);
+                BigDecimal subTax = new BigDecimal(0.0);
+                BigDecimal POtot = new BigDecimal(0.0);
+                try {
+                        document.add(new Paragraph("\n"));
+                        Optional<PurchaseOrder> optPurchaseOrder = purchaseOrderRepository
+                                        .findById(Long.parseLong(repid));
+                        if (optPurchaseOrder.isPresent()) {
+                                PurchaseOrder purchaseOrder = optPurchaseOrder.get();
+                                document.add(new Paragraph("Purchase Order# " + repid).setFont(font).setFontSize(18)
+                                                .setBold()
+                                                .setMarginTop(-30)
+                                                .setMarginLeft(150)
+                                                .setMarginRight(100)
+                                                .setTextAlignment(TextAlignment.RIGHT));
+                                document.add(new Paragraph("\n\n"));
+                                Optional<Vendor> optVendor = vendorRepository.findById(purchaseOrder.getVendorid());
+                                if (optVendor.isPresent()) {
+                                        Vendor vendor = optVendor.get();
+
+                                        Table table = new Table(2);
+                                        table.setWidth(new UnitValue(UnitValue.PERCENT, 30));
+                                        table.setMarginTop(20);
+
+                                        Cell cell = new Cell().add(new Paragraph("Vendor:")
+                                                        .setFont(font)
+                                                        .setFontSize(12)
+                                                        .setBold())
+                                                        .setBorder(Border.NO_BORDER)
+                                                        .setTextAlignment(TextAlignment.LEFT);
+                                        table.addCell(cell);
+                                        cell = new Cell().add(new Paragraph(vendor.getName())
+                                                        .setFont(font)
+                                                        .setFontSize(12)
+                                                        .setBold())
+                                                        .setBorder(Border.NO_BORDER)
+                                                        .setBackgroundColor(ColorConstants.LIGHT_GRAY)
+                                                        .setTextAlignment(TextAlignment.LEFT);
+                                        table.addCell(cell);
+                                        cell = new Cell().add(new Paragraph(" ")
+                                                        .setFont(font)
+                                                        .setFontSize(12))
+                                                        .setBorder(Border.NO_BORDER)
+                                                        .setTextAlignment(TextAlignment.LEFT);
+                                        table.addCell(cell);
+                                        cell = new Cell().add(new Paragraph(vendor.getAddress1())
+                                                        .setFont(font)
+                                                        .setFontSize(12)
+                                                        .setBold())
+                                                        .setBackgroundColor(ColorConstants.LIGHT_GRAY)
+                                                        .setBorder(Border.NO_BORDER)
+                                                        .setTextAlignment(TextAlignment.LEFT);
+                                        table.addCell(cell);
+                                        cell = new Cell().add(new Paragraph(" ")
+                                                        .setFont(font)
+                                                        .setFontSize(12))
+                                                        .setBorder(Border.NO_BORDER)
+                                                        .setTextAlignment(TextAlignment.LEFT);
+                                        table.addCell(cell);
+                                        cell = new Cell().add(new Paragraph(vendor.getCity())
+                                                        .setFont(font)
+                                                        .setFontSize(12)
+                                                        .setBold())
+                                                        .setBackgroundColor(ColorConstants.LIGHT_GRAY)
+                                                        .setBorder(Border.NO_BORDER)
+                                                        .setTextAlignment(TextAlignment.LEFT);
+                                        table.addCell(cell);
+                                        // table details
+                                        cell = new Cell().add(new Paragraph(" ")
+                                                        .setFont(font)
+                                                        .setFontSize(12))
+                                                        .setBorder(Border.NO_BORDER)
+                                                        .setTextAlignment(TextAlignment.LEFT);
+                                        table.addCell(cell);
+                                        cell = new Cell().add(new Paragraph(vendor.getProvince())
+                                                        .setFont(font)
+                                                        .setFontSize(12)
+                                                        .setBold())
+                                                        .setBackgroundColor(ColorConstants.LIGHT_GRAY)
+                                                        .setBorder(Border.NO_BORDER)
+                                                        .setTextAlignment(TextAlignment.LEFT);
+                                        table.addCell(cell);
+                                        cell = new Cell().add(new Paragraph(" ")
+                                                        .setFont(font)
+                                                        .setFontSize(12)
+                                                        .setBold())
+                                                        .setBorder(Border.NO_BORDER)
+                                                        .setTextAlignment(TextAlignment.RIGHT);
+                                        table.addCell(cell);
+                                        cell = new Cell().add(new Paragraph(vendor.getEmail())
+                                                        .setFont(font)
+                                                        .setFontSize(12)
+                                                        .setBold())
+                                                        .setBorder(Border.NO_BORDER)
+                                                        .setTextAlignment(TextAlignment.RIGHT)
+                                                        .setBackgroundColor(ColorConstants.LIGHT_GRAY);
+                                        table.addCell(cell);
+                                        document.add(table);
+                                } 
+                                document.add(new Paragraph("\n\n"));
+                                // now a 2 column table
+                                Table productTable = new Table(5);
+                                productTable.setWidth(new UnitValue(UnitValue.PERCENT, 100));
+
+                                Cell cell = new Cell().add(new Paragraph("Product Code")
+                                                .setFont(font)
+                                                .setFontSize(12)
+                                                .setBold())
+                                                .setTextAlignment(TextAlignment.CENTER);
+                                productTable.addCell(cell);
+                                cell = new Cell().add(new Paragraph("Description")
+                                                .setFont(font)
+                                                .setFontSize(12)
+                                                .setBold())
+                                                .setTextAlignment(TextAlignment.CENTER);
+                                productTable.addCell(cell);
+                                cell = new Cell().add(new Paragraph("Qty Sold")
+                                                .setFont(font)
+                                                .setFontSize(12)
+                                                .setBold())
+                                                .setTextAlignment(TextAlignment.CENTER);
+                                productTable.addCell(cell);
+                                cell = new Cell().add(new Paragraph("Price")
+                                                .setFont(font)
+                                                .setFontSize(12)
+                                                .setBold())
+                                                .setTextAlignment(TextAlignment.CENTER);
+                                productTable.addCell(cell);
+                                cell = new Cell().add(new Paragraph("Ext. Price")
+                                                .setFont(font)
+                                                .setFontSize(12)
+                                                .setBold())
+                                                .setTextAlignment(TextAlignment.CENTER);
+                                productTable.addCell(cell);
+                                for (PurchaseOrderLineitem line : purchaseOrder.getItems()) {
+                                        Optional<Product> optx = productRepository.findById(line.getProductid());
+                                        if (optx.isPresent()) {
+                                                Product product = optx.get();
+                                                // second row
+                                                cell = new Cell().add(new Paragraph(product.getId())
+                                                                .setFont(font)
+                                                                .setFontSize(12))
+                                                                .setTextAlignment(TextAlignment.CENTER);
+                                                productTable.addCell(cell);
+                                                // productTable details
+                                                cell = new Cell().add(new Paragraph(product.getName())
+                                                                .setFont(font)
+                                                                .setFontSize(12))
+                                                                .setTextAlignment(TextAlignment.CENTER);
+                                                productTable.addCell(cell);
+                                                cell = new Cell().add(new Paragraph(Integer.toString(line.getQty()))
+                                                                .setFont(font)
+                                                                .setFontSize(12))
+                                                                .setTextAlignment(TextAlignment.RIGHT);
+                                                productTable.addCell(cell);
+                                                cell = new Cell().add(
+                                                                new Paragraph(formatter.format(product.getCostprice()))
+                                                                                .setFont(font)
+                                                                                .setFontSize(12))
+                                                                .setTextAlignment(TextAlignment.RIGHT);
+                                                productTable.addCell(cell);
+
+                                                ExtPricetot = product.getCostprice().multiply(
+                                                                BigDecimal.valueOf(line.getQty()),
+                                                                new MathContext(8, RoundingMode.UP));
+                                                cell = new Cell().add(
+                                                                new Paragraph(formatter.format(ExtPricetot))
+                                                                                .setFont(font)
+                                                                                .setFontSize(12))
+                                                                .setTextAlignment(TextAlignment.RIGHT);
+                                                productTable.addCell(cell);
+                                                tot = tot.add(ExtPricetot, new MathContext(8, RoundingMode.UP));
+                                                subTax = tot.multiply(tax, new MathContext(8, RoundingMode.UP));
+                                                POtot = tot.add(subTax, new MathContext(8, RoundingMode.UP));
+
+                                        } // end if
+                                } // end for
+
+                                cell = new Cell(1, 4).add(new Paragraph("Sub Total:"))
+                                                .setBorder(Border.NO_BORDER)
+                                                .setTextAlignment(TextAlignment.RIGHT);
+                                productTable.addCell(cell);
+                                cell = new Cell().add(new Paragraph(formatter.format(tot)))
+                                                .setTextAlignment(TextAlignment.RIGHT)
+                                                .setBackgroundColor(ColorConstants.YELLOW);
+                                productTable.addCell(cell);
+
+                                cell = new Cell(1, 4).add(new Paragraph("Tax:"))
+                                                .setBorder(Border.NO_BORDER)
+                                                .setTextAlignment(TextAlignment.RIGHT);
+                                productTable.addCell(cell);
+                                cell = new Cell().add(new Paragraph(formatter.format(subTax)))
+                                                .setTextAlignment(TextAlignment.RIGHT)
+                                                .setBackgroundColor(ColorConstants.YELLOW);
+                                productTable.addCell(cell);
+
+                                cell = new Cell(1, 4).add(new Paragraph("PO Total:"))
+                                                .setBorder(Border.NO_BORDER)
+                                                .setTextAlignment(TextAlignment.RIGHT);
+                                productTable.addCell(cell);
+                                cell = new Cell().add(new Paragraph(formatter.format(POtot)))
+                                                .setTextAlignment(TextAlignment.RIGHT)
+                                                .setBackgroundColor(ColorConstants.YELLOW);
+                                productTable.addCell(cell);
+                                document.add(productTable);
+                                document.add(new Paragraph("\n\n"));
+                                DateTimeFormatter dateFormatter = DateTimeFormatter
+                                                .ofPattern("yyyy-MM-dd h:mm a");
+                                document.add(new Paragraph(dateFormatter.format(LocalDateTime.now()))
+                                                .setTextAlignment(TextAlignment.CENTER));
+                                document.close();
+                        }
+                } catch (Exception ex) {
+                        Logger.getLogger(PurchaseOrderPDFGenerator.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                return new ByteArrayInputStream(baos.toByteArray());
+        }
+}
